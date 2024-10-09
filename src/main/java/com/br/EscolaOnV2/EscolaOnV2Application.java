f28003d0e1cdf9e4dc4b6e6e6f6c1df4503736c6
limongi1234 <rntlemoslimongideaguiarmoraes@gmail.com>
2024-10-09 20:36:49 -0300

    Criação da nova versão do projeto, já com todas as dependências e arquitetura

---
+++ src/main/java/com/br/EscolaOnV2/EscolaOnV2Application.java
@@ -0,0 +1,13 @@
+package com.br.EscolaOnV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaOnV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaOnV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/EscolaOnV2/EscolaOnV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.EscolaOnV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaOnV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
